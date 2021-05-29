09c938c96bfa2e605b99e074715d6ce4a8af31d2
RanaJazim <stackdeveloper87@gmail.com>
2021-05-29 04:27:34 -0700

    signout the user from app ..

--- public/javascripts/header.js
+++ public/javascripts/header.js
@@ -1,27 +1,30 @@
 function toggleNavMenu() {
-    let navmenu = document.getElementsByTagName('nav')[0];
-    if (navmenu.hidden == false) {
-        navmenu.hidden = true;
-        window.removeEventListener("click", hideNav);
-    } else {
-        navmenu.hidden = false;
-        window.setTimeout(() => { window.addEventListener("click", hideNav); }, 0);
-    }
+  let navmenu = document.getElementsByTagName("nav")[0];
+  if (navmenu.hidden == false) {
+    navmenu.hidden = true;
+    window.removeEventListener("click", hideNav);
+  } else {
+    navmenu.hidden = false;
+    window.setTimeout(() => {
+      window.addEventListener("click", hideNav);
+    }, 0);
+  }
 }
 
 function hideNav(event) {
-    let width = window.matchMedia("(max-width: 768px)");
-    if (width.matches) {
-        toggleNavMenu();
-    }
+  let width = window.matchMedia("(max-width: 768px)");
+  if (width.matches) {
+    toggleNavMenu();
+  }
 }
 
 function signOut() {
-    document.cookie = "userType=;path=/";
-    window.location.href = "/login.html";
+  document.cookie = "userType=;path=/";
+  window.localStorage.removeItem("user");
+  window.location.href = "/login.html";
 }
 
-var header = document.getElementsByTagName('header')[0];
+var header = document.getElementsByTagName("header")[0];
 header.innerHTML = `<button onclick="toggleNavMenu()" id="nav-menu" class="pure-button"><i class="fas fa-bars"></i></button><button class='pure-button pure-button-primary sign-out' onclick="signOut()">Sign Out</button>
                     <div class="left">
                         <p class="left">Select user type (for testing purposes only):</p>
