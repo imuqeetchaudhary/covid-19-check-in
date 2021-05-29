fa4e2a2b222e2e68d52aa26619663fc329eb1302
RanaJazim <stackdeveloper87@gmail.com>
2021-05-28 22:37:02 -0700

    added dynamic urls for registration on client side ...

--- public/javascripts/register.js
+++ public/javascripts/register.js
@@ -1,25 +1,51 @@
 var vm = new Vue({
-    el: '#app',
-    data: {
-        venue_owner: false
-    }
+  el: "#app",
+  data: {
+    venue_owner: false,
+    credentials: {
+      name: "Jazim Abbas",
+      familyName: "",
+      email: "",
+      password: "",
+      phone: "",
+    },
+  },
+  methods: {
+    async onRegister() {
+      alert(this.venue_owner);
+      alert(this.registerURL);
+
+      // try {
+      //   const res = await axios.post("/user/register", {});
+      //   console.log(res);
+      // } catch (err) {
+      //   console.log(err.response);
+      // }
+      //   alert("user registration");
+    },
+  },
+  computed: {
+    registerURL() {
+      return this.venue_owner ? "/venue-owner/register" : "/user/register";
+    },
+  },
 });
 
 function back() {
-    window.location.href = "/login.html";
+  window.location.href = "/login.html";
 }
 
 function register() {
-    return true;
+  return true;
 }
 
 function validatePassword() {
-    var password = document.getElementById("password");
-    var confirmPassword = document.getElementById("password-confirm");
+  var password = document.getElementById("password");
+  var confirmPassword = document.getElementById("password-confirm");
 
-    if (String(password.value) != String(confirmPassword.value)) {
-        confirmPassword.setCustomValidity("Passwords must match.");
-    } else {
-        confirmPassword.setCustomValidity("");
-    }
+  if (String(password.value) != String(confirmPassword.value)) {
+    confirmPassword.setCustomValidity("Passwords must match.");
+  } else {
+    confirmPassword.setCustomValidity("");
+  }
 }
